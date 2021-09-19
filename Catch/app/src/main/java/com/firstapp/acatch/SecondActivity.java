7be782a384101d469793f8c059b305f90e38b18f
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2021-09-18 21:15:38 -0400

    Code template (Cite https://www.journaldev.com/9231/android-spinner-drop-down-list)

---
+++ Catch/app/src/main/java/com/firstapp/acatch/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.firstapp.acatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+/* The code comes from: https://www.journaldev.com/9231/android-spinner-drop-down-list
+ * More of our own code will be added later. */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        TextView  textView=(TextView) findViewById(R.id.txt_bundle);
+        Bundle bundle=getIntent().getExtras();
+        String data=bundle.get("data").toString();
+        textView.setText(data);
+    }
+}
